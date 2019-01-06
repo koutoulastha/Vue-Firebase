b50e79a1ba389bd4f866256383723575867bac73
Thanos Koutoulas <koutoulastha@hotmail.com>
2019-01-06 21:11:26 +0200

    Added authentication

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import TestComponent from '@/components/TestComponent'
+import Vue from 'vue';
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
+import TestComponent from '@/components/TestComponent';
+import Login from '@/components/Login';
+import firebase from 'firebase';
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/hello',
@@ -15,7 +17,61 @@ export default new Router({
     {
       path: '/',
       name: 'TestComponent',
-      component: TestComponent
+      component: TestComponent,
+      meta: {
+        requiresAuth: true
+      }
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        requiresGuest: true
+      }
+    }
   ]
 })
+
+//Nav guards
+router.beforeEach((to, from, next) => {
+  //Check for requiredAuth guard
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    //Check if NOT logged in
+    if(!firebase.auth().currentUser) {
+      //Go to login
+      next({
+        path: '/login',
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    }
+    else {
+      //proceed to route
+      next();
+    }
+  }
+  else if(to.matched.some(record => record.meta.requiresGuest)) {
+    //Check if logged in
+    if(firebase.auth().currentUser) {
+      //Go to home
+      next({
+        path: '/',
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    }
+    else {
+      //proceed to route
+      next();
+    }
+  }
+  else{
+    //proceed to route
+    next();
+  }
+});
+
+export default router;
